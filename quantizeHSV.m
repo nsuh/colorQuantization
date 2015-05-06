9c399f1ef0beae91d0fc148ba640c980a5ab8be8
Nathan Suh <nathansuh1@gmail.com>
2015-05-06 16:33:32 -0700

    update quantize HSV

--- quantizeHSV.m
+++ quantizeHSV.m
@@ -3,10 +3,10 @@ function [outputImg, meanHues] = quantizeHSV(origImg, k)
     xy = x*y;
     outputImg = rgb2hsv(origImg);
     stretch = reshape(outputImg, [xy, 1,z]);  %stretch matrix into vector
-    [assignedHue, valuesHue] = kmeans(double(stretch(:,:,1)), k);  %running kmeans
+    [assignedHue, meanHues] = kmeans(double(stretch(:,:,1)), k);  %running kmeans
     for i = 1:k  %for each cluster, assign the color
         i
-        assignedHue(assignedHue == i) = valuesHue(i);
+        assignedHue(assignedHue == i) = meanHues(i);
     end
     %assignedHue = uint8(assignedHue);  %!!!!
     outputImg(:,:,1) = reshape(assignedHue, [x,y,1]);
