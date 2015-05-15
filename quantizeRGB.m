f31cc62ab6083e94021e0b2b744f7fe8448541f4
Nathan Suh <nathansuh1@gmail.com>
2015-05-15 00:56:20 -0700

    fix quantize RGB

--- quantizeRGB.m
+++ quantizeRGB.m
@@ -1,24 +1,19 @@
 function [outputImg, meanColors] = quantizeRGB(origImg, k)
     [x, y, z] = size(origImg);
-    xy = x*y
-    stretch = reshape(origImg, [xy, 1,z]);
-    assignedR = zeros(xy, 1);
-    assignedG = zeros(xy, 1);
-    assignedB = zeros(xy, 1);
-    [assignedR, valuesR] = kmeans(double(stretch(:,:,1)), k);
-    [assignedG, valuesG] = kmeans(double(stretch(:,:,2)), k);
-    [assignedB, valuesB] = kmeans(double(stretch(:,:,3)), k);
+    xy = x*y;
+    stretch = reshape(origImg, [xy, 1,z]);  %stretch matrix into vector
+    colors(:,1) = double(stretch(:,:,1));
+    colors(:,2) = double(stretch(:,:,2));
+    colors(:,3) = double(stretch(:,:,3));
+    [pixelGroups, meanColors] = kmeans(colors, k);
     
-    for i = 1:k
-        assignedR(assignedR == i) = uint8(valuesR(i));
-        assignedG(assignedG == i) = uint8(valuesG(i));
-        assignedB(assignedB == i) = uint8(valuesB(i));
+    for i = 1:k  %for each color
+         colors(pixelGroups == i, :) = repmat(meanColors(i, :), sum(pixelGroups == i), 1);  %replace original colors with quantized
     end
-    outputImg = zeros(xy, 1, z);
-    outputImg(:,:,1) = assignedR;
-    outputImg(:,:,2) = assignedG;
-    outputImg(:,:,3) = assignedB;
-    outputImg = reshape(outputImg, [x,y,z]);
+    
+    outputImg = reshape(colors, [x,y,z]);
     outputImg = uint8(outputImg);
     imshow(outputImg);
     
+    
+
