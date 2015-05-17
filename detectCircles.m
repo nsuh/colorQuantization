ebfe3e1765a7497716e8c4361869ca7f4a0a7ba9
Nathan Suh <nathansuh1@gmail.com>
2015-05-17 04:33:34 -0700

    its late

--- detectCircles.m
+++ detectCircles.m
@@ -61,3 +61,34 @@ function [centers] = detectCircles(im, radius, useGradient)
 %         end
 %     end
 %     
+
+%even less old function
+% function [centers] = detectCircles(im, radius, useGradient)
+%     [xDim, yDim, zDim] = size(im);
+%     [fx, fy] = imgradient(rgb2gray(im));
+%     edges = edge(rgb2gray(im), 'canny');
+%     [x,y] = find(edges == 1);
+%     [numEdgePoints, junk] = size(find(edges == 1));
+%     houghSpace = zeros(xDim + 2*radius, yDim + 2*radius);
+%     if useGradient == 0
+%         for pixel = 1:numEdgePoints
+%             for theta = 1:360
+%                 a = ceil(x(pixel) - radius*cos(degtorad(theta))) + radius;
+%                 b = ceil(y(pixel) + radius*sin(degtorad(theta))) + radius;
+%                 houghSpace(a,b) = houghSpace(a,b) + 1;
+%             end
+%         end
+%     elseif useGradient == 1
+%         for pixel = 1:numEdgePoints
+%             theta = atan2(fy(x(pixel), y(pixel)), fx(x(pixel), y(pixel)));
+%             a = ceil(x(pixel) - radius*cos(degtorad(theta))) + radius;
+%             b = ceil(y(pixel) + radius*sin(degtorad(theta))) + radius;
+%             houghSpace(a,b) = houghSpace(a,b) + 1;
+%         end
+%     end
+%     centers = houghpeaks(houghSpace, 5)
+%     
+%     imshow(uint8(houghSpace))
+%     imshow(houghSpace)
+%     hold on
+%     scatter(centers(:,1), centers(:,2))
